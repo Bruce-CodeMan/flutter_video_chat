73b101952acbeaad454628736f638a8b7a522f82
Bruce Hsu <1033684650@qq.com>
2024-04-14 20:14:41 +0800

    modified the voicecall page

--- lib/pages/message/voicecall/view.dart
+++ lib/pages/message/voicecall/view.dart
@@ -14,8 +14,152 @@ class VoiceCallPage extends GetView<VoiceCallController> {
     return Scaffold(
       appBar: AppBar(title: const Text("voiceCall")),
       backgroundColor: AppColors.primaryThirdBackground,
-      body: Obx(()=>Container(
-        child: Stack(
+      body: Obx(() => _buildBody())
+    );
+  }
+  // 聊天时长
+  Widget _buildChatTime() {
+    return Text(
+            controller.state.callTime.value,
+            style: TextStyle(
+              color: AppColors.primaryElementText,
+              fontSize: 14.sp,
+              fontWeight: FontWeight.normal
+            ),
+          );
+  }
+
+  // 头像
+  Widget _buildAvatar() {
+    return Container(
+            width: 70.h,
+            height: 70.h,
+            margin: EdgeInsets.only(top: 150.h),
+            child: Image.network(controller.state.toAvatar.value),
+          );
+  }
+
+  Widget _buildName() {
+    return Container(
+            margin: EdgeInsets.only(top: 20.h, left: 30.w, right: 30.w),
+            child: Text(
+              controller.state.toName.value,
+              style: TextStyle(
+                color: AppColors.primaryElementText,
+                fontSize: 18.sp,
+                fontWeight: FontWeight.bold
+              ),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+          );
+}
+
+  // 下方的三个按钮
+  Widget _buildMicrophoneSection() {
+    return Column(
+              children: [
+                GestureDetector(
+                  child: Container(
+                    padding: EdgeInsets.all(15.w),
+                    width: 60.w,
+                    height: 60.w,
+                    decoration: BoxDecoration(
+                      color: controller.state.openMicrophone.value
+                        ? AppColors.primaryElementText
+                        : AppColors.primaryText,
+                      borderRadius: BorderRadius.all(Radius.circular(30.w))
+                    ),
+                    child: controller.state.openMicrophone.value
+                      ? Image.asset(Asset.getIconPath("b_microphone"))
+                      : Image.asset(Asset.getIconPath("a_microphone")),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 10.h),
+                  child: Text(
+                    "Microphone",
+                    style: TextStyle(
+                      color: AppColors.primaryElementText,
+                      fontSize: 12.sp,
+                      fontWeight: FontWeight.normal
+                    ),
+                  ),
+                )
+              ],
+            );
+  }
+
+  Widget _buildphoneSection() {
+    return Column(
+            children: [
+              GestureDetector(
+                child: Container(
+                  padding: EdgeInsets.all(15.w),
+                  width: 60.w,
+                  height: 60.w,
+                  decoration: BoxDecoration(
+                    color: controller.state.isJoined.value
+                      ? AppColors.primaryElementBg
+                      : AppColors.primaryElementStatus,
+                    borderRadius: BorderRadius.all(Radius.circular(30.w))
+                  ),
+                  child: controller.state.isJoined.value
+                    ? Image.asset(Asset.getIconPath("a_phone"))
+                    : Image.asset(Asset.getIconPath("a_telephone")),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10.h),
+                child: Text(
+                  controller.state.isJoined.value ? "Disconnect": "Connect",
+                  style: TextStyle(
+                    color: AppColors.primaryElementText,
+                    fontSize: 12.sp,
+                    fontWeight: FontWeight.normal
+                  ),
+                ),
+              )
+            ],
+          );
+  }
+
+  Widget _buildSpeakerSection() {
+    return Column(
+            children: [
+              GestureDetector(
+                child: Container(
+                  padding: EdgeInsets.all(15.w),
+                  width: 60.w,
+                  height: 60.w,
+                  decoration: BoxDecoration(
+                    color: controller.state.enableSpeaker.value
+                      ? AppColors.primaryElementText
+                      : AppColors.primaryText,
+                    borderRadius: BorderRadius.all(Radius.circular(30.w))
+                  ),
+                  child: controller.state.enableSpeaker.value
+                    ? Image.asset(Asset.getIconPath("b_trumpet"))
+                    : Image.asset(Asset.getIconPath("a_trumpet")),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10.h),
+                child: Text(
+                  "Speaker",
+                  style: TextStyle(
+                    color: AppColors.primaryElementText,
+                    fontSize: 12.sp,
+                    fontWeight: FontWeight.normal
+                  ),
+                ),
+              )
+            ],
+          );
+  }
+
+  Widget _buildBody() {
+    return Stack(
           children: [
             Positioned(
               top: 10.h,
@@ -23,31 +167,9 @@ class VoiceCallPage extends GetView<VoiceCallController> {
               right: 30.w,
               child: Column(
                 children: [
-                  Text(
-                    controller.state.callTime.value,
-                    style: TextStyle(
-                      color: AppColors.primaryElementText,
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.normal
-                    ),
-                  ),
-                  Container(
-                    width: 70.h,
-                    height: 70.h,
-                    margin: EdgeInsets.only(top: 150.h),
-                    child: Image.network(controller.state.toAvatar.value),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 10.h),
-                    child: Text(
-                      controller.state.toName.value,
-                      style: TextStyle(
-                        color: AppColors.primaryElementText,
-                        fontSize: 18.sp,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                  )
+                  _buildChatTime(),
+                  _buildAvatar(),
+                  _buildName()
                 ],
               )
             ),
@@ -59,107 +181,13 @@ class VoiceCallPage extends GetView<VoiceCallController> {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   // microPhone section
-                  Column(
-                    children: [
-                      GestureDetector(
-                        child: Container(
-                          padding: EdgeInsets.all(15.w),
-                          width: 60.w,
-                          height: 60.w,
-                          decoration: BoxDecoration(
-                            color: controller.state.openMicrophone.value
-                              ? AppColors.primaryElementText
-                              : AppColors.primaryText,
-                            borderRadius: BorderRadius.all(Radius.circular(30.w))
-                          ),
-                          child: controller.state.openMicrophone.value
-                            ? Image.asset(Asset.getIconPath("b_microphone"))
-                            : Image.asset(Asset.getIconPath("a_microphone")),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 10.h),
-                        child: Text(
-                          "Microphone",
-                          style: TextStyle(
-                            color: AppColors.primaryElementText,
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.normal
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      GestureDetector(
-                        child: Container(
-                          padding: EdgeInsets.all(15.w),
-                          width: 60.w,
-                          height: 60.w,
-                          decoration: BoxDecoration(
-                            color: controller.state.isJoined.value
-                              ? AppColors.primaryElementBg
-                              : AppColors.primaryElementStatus,
-                            borderRadius: BorderRadius.all(Radius.circular(30.w))
-                          ),
-                          child: controller.state.isJoined.value
-                            ? Image.asset(Asset.getIconPath("a_phone"))
-                            : Image.asset(Asset.getIconPath("a_telephone")),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 10.h),
-                        child: Text(
-                          controller.state.isJoined.value ? "Disconnect": "Connect",
-                          style: TextStyle(
-                            color: AppColors.primaryElementText,
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.normal
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      GestureDetector(
-                        child: Container(
-                          padding: EdgeInsets.all(15.w),
-                          width: 60.w,
-                          height: 60.w,
-                          decoration: BoxDecoration(
-                            color: controller.state.enableSpeaker.value
-                              ? AppColors.primaryElementText
-                              : AppColors.primaryText,
-                            borderRadius: BorderRadius.all(Radius.circular(30.w))
-                          ),
-                          child: controller.state.enableSpeaker.value
-                            ? Image.asset(Asset.getIconPath("b_trumpet"))
-                            : Image.asset(Asset.getIconPath("a_trumpet")),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 10.h),
-                        child: Text(
-                          "Speaker",
-                          style: TextStyle(
-                            color: AppColors.primaryElementText,
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.normal
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                  _buildMicrophoneSection(),
+                  _buildphoneSection(),
+                  _buildSpeakerSection()
                 ],
               )
             ) 
           ],
-        ),
-      ))
-    );
+        );
   }
-
-  
 }
